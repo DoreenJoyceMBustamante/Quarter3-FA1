2d83a60043ea7daf6876ad485c403beeca30da4f
DoreenJoyceMBustamante <doreenjoyce.bustamante@smc.pshs.edu.ph>
2025-01-29 16:03:41 +0800

    Update prob1.js
    Add missing code

--- prob1.js
+++ prob1.js
@@ -1,6 +1,10 @@
-var name = "Doreen";
-var h = "62";
-var w = "45";
-alert("Name: " + name);
-alert("Height: " + h);
-alert("Weight: " + w);
+let nickname = "Doreen";
+let height = 62;  
+let weight = 45;    
+
+let feet = parseInt(height / 12); 
+let inches = parseInt(height % 12); 
+
+let finalWeight = weight * 2.205.toFixed(3);  
+
+alert(`Name: ${nickname}\nHeight: ${feet}’${inches}”\nWeight: ${finalWeight.toFixed(3)} lbs`);
